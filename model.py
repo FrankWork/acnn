05f29e1f1b627f8de2e45d38d1e23f69978f1b0e
FrankWork <lzh00776@163.com>
2017-05-08 19:39:14 +0800

    baseline

--- model.py
+++ model.py
@@ -38,24 +38,6 @@ class Model(object):
     dist2 = tf.nn.embedding_lookup(pos2_embed, in_dist2, name='dist2')# bz, n, k,dp
     y = tf.nn.embedding_lookup(rel_embed, in_y, name='y')# bz, dc
 
-
-    # input attention
-    def inner_product(e, x):
-      '''
-      <x, y> = x1y1 + x2y2 + ... + xnyn
-      e:        (bz, dw) => (bz, 1, dw)
-      x:        (bz, n, dw)
-      return :  (bz, n)
-      '''
-      return tf.reduce_sum(
-                tf.multiply(tf.reshape(e, [bz, 1, dw]), x), 
-                -1
-              )
-
-    alpha1 = tf.nn.softmax(inner_product(e1, x))# bz, n
-    alpha2 = tf.nn.softmax(inner_product(e2, x))# bz, n
-    alpha = (alpha1 + alpha2)/2
-
     # convolution
     # x: (batch_size, max_len, embdding_size, 1)
     # w: (filter_size, embdding_size, 1, num_filters)
@@ -68,80 +50,37 @@ class Model(object):
     w = tf.get_variable(initializer=initializer,shape=[k, d, 1, dc],name='weight')
     b = tf.get_variable(initializer=initializer,shape=[dc],name='bias')
     conv = tf.nn.conv2d(x_conv, w, strides=[1,1,d,1],padding="SAME")
-    r = tf.multiply(tf.reshape(conv, [bz, n, dc]), tf.reshape(alpha, [bz, n, 1])) # bz, n, 1, dc
 
     R = tf.nn.tanh(tf.nn.bias_add(r,b),name="R") # bz, n, 1, dc
     R = tf.reshape(R, [bz, n, dc])
 
-    # attention pooling
-    U = tf.get_variable(initializer=initializer,shape=[dc,dc],name='U')
-    
-    # batch matmul
-    # G = R * U * WL
-    # R: (bz, n, dc)
-    # U: (dc, dc)
-    # WL:(dc, nr)
-    G = tf.matmul(# (bz*n,dc), (dc, dc) => (bz*n, dc)
-      tf.reshape(R, [bz*n, dc]), U
-    )
-    G = tf.matmul(# (bz*n, dc), (dc, nr) => (bz*n, nr)
-      G, tf.transpose(rel_embed)
-    ) 
-    G = tf.reshape(G, [bz, n, nr])
-    AP = tf.nn.softmax(G, dim=1)# attention pooling tensor
-
-    # predict
-    wo = tf.matmul(
-      tf.transpose(R, perm=[0, 2, 1]), # batch transpose: (bz, n, dc) => (bz,dc,n)
-      AP
-    )# (bz, dc, nr)
-    wo = tf.reduce_max(wo, axis=-1) # (bz, dc)
-    # wo = tf.nn.max_pool(tf.expand_dims(wo,-1),
-    #                     ksize=[1,1,nr,1],
-    #                     strides=[1,1,nr,1],
-    #                     padding="SAME"
-    #       )# (bz, dc, 1, 1)
-    # wo=tf.reshape(wo,[bz, dc])
-
+    # max pooling
+    pooled = tf.nn.max_pool(tf.expand_dims(R, axis=-1),
+                        ksize=[1,n,1,1],
+                        strides=[1,n,1,1],
+                        padding="SAME"
+          )
+    h_pool_flat = tf.reshape(pooled,[-1,dc])
     if is_training and keep_prob < 1:
-      wo = tf.nn.dropout(wo, keep_prob)
-
-    # accuracy
-    def distance(wo, y, axis=None):
-      return tf.norm(
-                tf.nn.l2_normalize(wo, dim=-1) - y,
-                axis = axis
-            )# a scalar value
-
-
-    all_dist = distance(
-              tf.tile(tf.expand_dims(wo, axis=1), [1, nr, 1]), # bz, nr, dc
-              rel_embed,# nr, dc
-              axis=-1
-    )# bz, nr
-    predict = tf.argmin(all_dist, axis=-1)
+      h_pool_flat = tf.nn.dropout(h_pool_flat, keep_prob)
+    W_o = tf.get_variable(initializer=initializer,shape=[dc,nr],name='w_o')
+    b_o = tf.get_variable(initializer=initializer,shape=[nr],name='b_o')
+    scores = tf.nn.xw_plus_b(h_pool_flat,W_o,b_o,name="scores")
+    predict = tf.argmax(scores,1,name="predictions")
     predict = tf.cast(predict, dtype=tf.int32)
     acc = tf.reduce_sum(tf.cast(tf.equal(predict, in_y), dtype=tf.int32))
     self.predict = predict
     self.acc = acc
-
+    loss = tf.reduce_mean(
+      tf.nn.softmax_cross_entropy_with_logits(logits=scores, 
+                                              labels=tf.one_hot(in_y, nr))
+    )
+    l2_loss = tf.nn.l2_loss(W_o)
+    l2_loss += tf.nn.l2_loss(b_o)
+    l2_loss = config.l2_reg_lambda * l2_loss
+    
     if not is_training:
       return
-
-    # train
-    mask = tf.one_hot(in_y, nr, on_value=1000., off_value=0.)# bz, nr
-    neg_dist = tf.add(all_dist, mask)
-    neg_y = tf.argmin(neg_dist, axis=-1)# bz, 1
-    neg_y = tf.nn.embedding_lookup(rel_embed, neg_y)# bz, dc
-
-    l2_loss = tf.nn.l2_loss(rel_embed)
-    l2_loss += tf.nn.l2_loss(U)
-    l2_loss += tf.nn.l2_loss(w)
-    l2_loss += tf.nn.l2_loss(b)
-    l2_loss = config.l2_reg_lambda * l2_loss
-
-    loss = distance(wo, y) + (config.margin - distance(wo, neg_y))
-    self.loss = loss
 
     # optimizer 
     # optimizer = tf.train.GradientDescentOptimizer(config.learning_rate)
@@ -162,30 +101,6 @@ class Model(object):
 
     self.train_op = train_op
     self.reg_op = reg_op
-    # self.reg_op = tf.no_op()
     self.global_step = global_step
 
-    # max pooling
-    # pooled = tf.nn.max_pool(tf.expand_dims(R, axis=-1),
-    #                     ksize=[1,n,1,1],
-    #                     strides=[1,n,1,1],
-    #                     padding="SAME"
-    #       )
-    # h_pool_flat = tf.reshape(pooled,[-1,dc])
-    # if is_training and keep_prob < 1:
-    #   h_pool_flat = tf.nn.dropout(h_pool_flat, keep_prob)
-    # W_o = tf.get_variable(initializer=initializer,shape=[dc,nr],name='w_o')
-    # b_o = tf.get_variable(initializer=initializer,shape=[nr],name='b_o')
-    # scores = tf.nn.xw_plus_b(h_pool_flat,W_o,b_o,name="scores")
-    # predict = tf.argmax(scores,1,name="predictions")
-    # predict = tf.cast(predict, dtype=tf.int32)
-    # acc = tf.reduce_sum(tf.cast(tf.equal(predict, in_y), dtype=tf.int32))
-    # self.predict = predict
-    # self.acc = acc
-    # loss = tf.reduce_mean(
-    #   tf.nn.softmax_cross_entropy_with_logits(logits=scores, 
-    #                                           labels=tf.one_hot(in_y, nr))
-    # )
-    # l2_loss = tf.nn.l2_loss(W_o)
-    # l2_loss += tf.nn.l2_loss(b_o)
-    # l2_loss = config.l2_reg_lambda * l2_loss
+    
